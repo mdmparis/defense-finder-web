e21ebc408925bc44340fee1dfce6688a5ae01c0b
Alexandre Hervé <alexandreherve@protonmail.com>
2021-07-21 22:55:17 +0200

    Begin app setup with tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { ProteinForm } from './ProteinForm/ProteinForm'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex flex-col">
+        <header className="mb-8 p-5 flex flex-row justify-between border-b">
+          <div>
+            Defense Finder
+          </div>
+          <div>
+            By MDM Labs, Paris
+          </div>
+        </header>
+        <ProteinForm />
+      </div>
     </div>
   );
 }
---
+++ src/ProteinForm/ProteinForm.js
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './ProteinForm.css'
+
+export function ProteinForm() {
+  const [protein, setProtein] = useState()
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log('e', e)
+  }
+
+  return (
+    <div class="container mx-auto">
+      <form onSubmit={onSubmit}>
+        <div className="flex flex-col mx-10">
+          <textarea
+            className="textarea border rounded mb-5 p-4 focus:border-blue-500"
+            rows="15"
+            name="protein"
+            value={protein}
+            onChange={e => setProtein(e.target.value)}
+            placeholder="Paste your protein sequence for defense systems analysis"
+          >
+          </textarea>
+          <button type="submit" className="p-4 border text-blue-700 rounded bg-blue-100 hover:bg-blue-500 focus:bg-blue-700 hover:text-white focus:text-white">
+            Find defense systems
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
