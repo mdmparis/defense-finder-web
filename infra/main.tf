612b37fa7dc0fed8a69d9b9449440a754e0a85d0
Alexandre Hervé <alexandreherve@protonmail.com>
2021-08-13 12:25:23 +0200

    Init terraform infra

---
+++ infra/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+
+  backend "remote" {
+    organization = "mdmparis"
+    workspaces {
+      name = "defense-finder-web"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-3"
+}
+
+resource "aws_s3_bucket" "proteins_bucket" {
+  bucket = "proteins"
+  acl    = "private"
+}
