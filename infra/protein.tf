fe60ee7026debcf26e38d874f62a992641e85b23
Alexandre Hervé <alexandreherve@protonmail.com>
2021-08-13 14:46:24 +0200

    Add upload policy

--- infra/protein.tf
+++ infra/protein.tf
@@ -4,17 +4,35 @@ resource "aws_s3_bucket" "proteins_bucket" {
 }
 
 #resource "aws_iam_role" "protein_upload_role" {
-  #name = "protein_upload_role"
+#name = "protein_upload_role"
 
-  #assume_role_policy = jsonencode({
-    #Version = "2012-10-17"
-    #Statement = [
-      #{
-        #Effect   = "Allow",
-        #Action   = "s3:PutObject",
-        #Sid      = "",
-        #Resource = "${aws_s3_bucket.proteins_bucket.arn}/*"
-      #}
-    #]
-  #})
+#assume_role_policy = jsonencode({
+#Version = "2012-10-17"
+#Statement = [
+#{
+#Effect   = "Allow",
+#Action   = "s3:PutObject",
+#Sid      = "",
+#Resource = "${aws_s3_bucket.proteins_bucket.arn}/*"
 #}
+#]
+#})
+#}
+
+resource "aws_iam_policy" "upload-to-proteins-policy" {
+  name        = "upload-to-df-proteins"
+  path        = "/"
+  description = ""
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "VisualEditor0",
+        Effect   = "Allow",
+        Action   = "s3:PutObject",
+        Resource = "${aws_s3_bucket.proteins_bucket.arn}/*"
+      }
+    ]
+  })
+}
