dc2241e2b8015d3f202c9287f9b14535953613b0
Alexandre Hervé <alexandreherve@protonmail.com>
2021-08-13 16:39:02 +0200

    Remove custom handler roles

--- infra/handler.tf
+++ infra/handler.tf
@@ -1,34 +1,34 @@
 module "lambda_function_container_image" {
   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "protein_handler"
+  function_name = "protein_handler_lambda"
   description   = ""
 
   create_package = false
 
-  image_uri    = "public.ecr.aws/j8j2r4q6/mdmparis/defense-finder:latest"
+  image_uri    = "187971905951.dkr.ecr.eu-west-3.amazonaws.com/mdmparis/defense-finder:latest"
   package_type = "Image"
 }
 
-resource "aws_iam_role" "protein_handler_role" {
-  name = "protein_handler"
+#resource "aws_iam_role" "protein_handler_role" {
+  #name = "protein_handler"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
+  #assume_role_policy = jsonencode({
+    #Version = "2012-10-17"
+    #Statement = [
+      #{
+        #Action = "sts:AssumeRole"
+        #Effect = "Allow"
+        #Sid    = ""
+        #Principal = {
+          #Service = "lambda.amazonaws.com"
+        #}
+      #},
+    #]
+  #})
+#}
 
-resource "aws_iam_role_policy_attachment" "upload_to_results_policy_attachment" {
-  role       = aws_iam_role.protein_handler_role.name
-  policy_arn = aws_iam_policy.upload_to_results_policy.arn
-}
+#resource "aws_iam_role_policy_attachment" "upload_to_results_policy_attachment" {
+  #role       = aws_iam_role.protein_handler_role.name
+  #policy_arn = aws_iam_policy.upload_to_results_policy.arn
+#}
