2f3e0a70b5e8c32924307cda899e88bb2318beab
Alexandre Hervé <alexandreherve@protonmail.com>
2021-08-14 18:14:47 +0200

    Setup handler docker

--- handler/app.py
+++ handler/app.py
@@ -2,4 +2,8 @@ def handler(event, context):
     s3 = event['Records'][0]['s3']
     bucket = s3['bucket']['name']
     object = s3['object']['key']
+    # download the protein file
+    # run defense-finder on it
+    # upload the result to the results bucket
+    # delete the protein file on s3
     return "Hello {}/{}".format(bucket, object)
