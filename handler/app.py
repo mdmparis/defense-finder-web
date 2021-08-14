db78947ba7510e3faecc1982a8fd408ebb053ff8
Alexandre Hervé <alexandreherve@protonmail.com>
2021-08-14 12:16:14 +0200

    Improve app.py

--- handler/app.py
+++ handler/app.py
@@ -1,2 +1,5 @@
 def handler(event, context):
-    return "Hello {}".format(event['protein'])
+    s3 = event['Records'][0]['s3']
+    bucket = s3['bucket']['name']
+    object = s3['object']['key']
+    return "Hello {}/{}".format(bucket, object)
