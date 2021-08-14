d9b483f4c1eae665236525b815da90b54237db40
Alexandre Hervé <alexandreherve@protonmail.com>
2021-08-14 11:20:21 +0200

    Set publish to true for lambda

--- infra/handler.tf
+++ infra/handler.tf
@@ -5,6 +5,7 @@ module "lambda_function_container_image" {
   description   = ""
 
   create_package = false
+  publish        = true
 
   image_uri    = "187971905951.dkr.ecr.eu-west-3.amazonaws.com/mdmparis/defense-finder:latest"
   package_type = "Image"
@@ -20,25 +21,25 @@ module "lambda_function_container_image" {
   }
 }
 
-#resource "aws_iam_role" "protein_handler_role" {
-  #name = "protein_handler"
+resource "aws_iam_role" "protein_handler_role" {
+  name = "protein_handler"
 
-  #assume_role_policy = jsonencode({
-    #Version = "2012-10-17"
-    #Statement = [
-      #{
-        #Action = "sts:AssumeRole"
-        #Effect = "Allow"
-        #Sid    = ""
-        #Principal = {
-          #Service = "lambda.amazonaws.com"
-        #}
-      #},
-    #]
-  #})
-#}
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
 
-#resource "aws_iam_role_policy_attachment" "upload_to_results_policy_attachment" {
-  #role       = aws_iam_role.protein_handler_role.name
-  #policy_arn = aws_iam_policy.upload_to_results_policy.arn
-#}
+resource "aws_iam_role_policy_attachment" "upload_to_results_policy_attachment" {
+  role       = aws_iam_role.protein_handler_role.name
+  policy_arn = aws_iam_policy.upload_to_results_policy.arn
+}
