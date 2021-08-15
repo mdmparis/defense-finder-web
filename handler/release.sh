657a8448fe91637035da81ad4b5c0e8238bc77fa
Alexandre Hervé <alexandreherve@protonmail.com>
2021-08-15 10:45:03 +0200

    Add signup to release.sh

--- handler/release.sh
+++ handler/release.sh
@@ -5,6 +5,7 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+aws ecr get-login-password --region eu-west-3 | docker login --username AWS --password-stdin 187971905951.dkr.ecr.eu-west-3.amazonaws.com
 docker build -t mdmparis/defense-finder .
 docker tag mdmparis/defense-finder:latest 187971905951.dkr.ecr.eu-west-3.amazonaws.com/mdmparis/defense-finder:$1
 docker push 187971905951.dkr.ecr.eu-west-3.amazonaws.com/mdmparis/defense-finder:$1
