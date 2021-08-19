42d04b120ad6afedac45f76ee5df7a3115ffde67
Aurélien Hervé <mail@aurelien-herve.com>
2021-08-19 10:18:28 +0200

    fix(s3): improve buckets security
    add proper access to the s3 buckets, instead of granting them to anyone

--- infra/protein.tf
+++ infra/protein.tf
@@ -18,24 +18,6 @@ resource "aws_s3_bucket" "proteins_bucket" {
   }
 }
 
-resource "aws_iam_policy" "upload_to_proteins_policy" {
-  name        = "upload-to-df-proteins"
-  path        = "/"
-  description = ""
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid      = "",
-        Effect   = "Allow",
-        Action   = "s3:PutObject",
-        Resource = "${aws_s3_bucket.proteins_bucket.arn}/*"
-      }
-    ]
-  })
-}
-
 resource "aws_iam_policy" "read_proteins_policy" {
   name        = "read-df-proteins"
   path        = "/"
