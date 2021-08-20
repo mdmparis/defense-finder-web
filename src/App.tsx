e543cfb9f0789df2a4aad0cdacd9f3c9f5cb4415
Alexandre Hervé <alexandreherve@protonmail.com>
2021-08-20 13:37:21 +0200

    Use history for logo nav

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { ProteinForm } from './ProteinForm/ProteinForm'
 import { Result } from './Results/Results'
 import { Logo } from './Logo'
+import { useHistory } from "react-router-dom";
 
 import {
   BrowserRouter as Router,
@@ -9,32 +10,39 @@ import {
   Route,
 } from "react-router-dom";
 
+function AppContent() {
+  const history = useHistory()
+  return (
+    <div className="App">
+      <div className="flex flex-col">
+        <header className="mb-8 p-5 flex flex-row justify-between items-center border-b">
+          <div>
+            <button onClick={() => history.push("/")}><Logo /></button>
+          </div>
+          <div>
+            <div className="text-sm text-gray-400">
+              Systematic search of all known anti-phage systems
+            </div>
+            <div className="text-sm text-shrimp">By MDM Labs, Paris</div>
+          </div>
+        </header>
+        <Switch>
+          <Route path="/result/:result">
+            <Result />
+          </Route>
+          <Route path="/">
+            <ProteinForm />
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <div className="flex flex-col">
-          <header className="mb-8 p-5 flex flex-row justify-between items-center border-b">
-            <div>
-              <a href="/"><Logo /></a>
-            </div>
-            <div>
-              <div className="text-sm text-gray-400">
-                Systematic search of all known anti-phage systems
-              </div>
-              <div className="text-sm text-shrimp">By MDM Labs, Paris</div>
-            </div>
-          </header>
-          <Switch>
-            <Route path="/result/:result">
-              <Result />
-            </Route>
-            <Route path="/">
-              <ProteinForm />
-            </Route>
-          </Switch>
-        </div>
-      </div>
+      <AppContent />
     </Router>
   )
 }
