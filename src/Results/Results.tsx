41e0228aaaaac8869c5a0348adb57b5f844636c8
Alexandre Hervé <alexandreherve@protonmail.com>
2021-09-01 20:54:08 +0200

    Format error

--- src/Results/Results.tsx
+++ src/Results/Results.tsx
@@ -51,9 +51,9 @@ const Err = ({error}: {error: string}) => {
     <div className="border p-4">
       <div className="text-lg text-shrimp mb-2">Something went wrong</div>
       <div className="mb-2">DefenseFinder couldn't handle your file. This generally occurs when MacSyFinder can't handle the uploaded file. Please take the following error message in account and if ever the problem persists, feel free to <a className="text-shrimp" href="https://github.com/mdmparis/defense-finder-web/issues">open an issue</a>.</div>
-      <pre className="border bg-gray-100">
+      <div className="border bg-gray-100">
         {error}
-      </pre>
+      </div>
     </div>
   )
 }
