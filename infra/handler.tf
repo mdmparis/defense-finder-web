ffa180a7ffe38eb0ecab4888d635175b8adb44a0
Alexandre Hervé <alexandreherve@protonmail.com>
2021-09-01 20:46:57 +0200

    Use handler 2.13

--- infra/handler.tf
+++ infra/handler.tf
@@ -7,7 +7,7 @@ module "lambda_function_container_image" {
   create_package = false
   publish        = true
 
-  image_uri    = "187971905951.dkr.ecr.eu-west-3.amazonaws.com/mdmparis/defense-finder:2.12"
+  image_uri    = "187971905951.dkr.ecr.eu-west-3.amazonaws.com/mdmparis/defense-finder:2.13"
   package_type = "Image"
 
   attach_policies    = true
