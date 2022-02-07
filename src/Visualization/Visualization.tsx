0d5284f5a8ed55e2e4d53b19042e3356675a3b37
Alexandre Hervé <alexandre.herve9@gmail.com>
2022-02-07 16:58:43 +0100

    fix contig box height, overflow scroll

--- src/Visualization/Visualization.tsx
+++ src/Visualization/Visualization.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useRef } from 'react'
-import { drawSystems } from './visualization'
+import { useEffect, useRef } from "react";
+import { drawSystems } from "./visualization";
 
 export const Visualization = ({
   contigData,
   systemData,
 }: {
-  contigData: object
-  systemData: object
+  contigData: object;
+  systemData: object;
 }) => {
-  const contigBoxRef = useRef(null)
-  const geneBoxRef = useRef(null)
+  const contigBoxRef = useRef(null);
+  const geneBoxRef = useRef(null);
 
   useEffect(() => {
     return drawSystems(
@@ -17,21 +17,29 @@ export const Visualization = ({
       geneBoxRef.current,
       contigData,
       systemData
-    )
-  }, [contigData, systemData])
+    );
+  }, [contigData, systemData]);
 
   return (
     <div
       className="border mb-6"
       style={{
-        position: 'relative',
-        width: '100%',
-        display: 'grid',
-        gridTemplateColumns: '4fr 6fr',
+        position: "relative",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "4fr 6fr",
       }}
     >
-      <div ref={contigBoxRef} style={{ background: '#000' }}></div>
-      <div ref={geneBoxRef} style={{ background: '#FBFCFC' }}></div>
+      <div
+        ref={contigBoxRef}
+        style={{
+          background: "#000",
+          overflowY: "scroll",
+          overflowX: "hidden",
+          height: "600px",
+        }}
+      ></div>
+      <div ref={geneBoxRef} style={{ background: "#FBFCFC" }}></div>
     </div>
-  )
-}
+  );
+};
--- src/Visualization/visualization.js
+++ src/Visualization/visualization.js
@@ -1,30 +1,29 @@
-import * as d3 from 'd3'
+import * as d3 from "d3";
 
-const arrow_tip_length = 10
-const arrow_height = 30
-const global_top = 50
-const geneSpacing = 5
-const arc_pad = 0.003 //used to make sure the systems are visible on the screen
-const margin = { top: 20, right: 20, bottom: 20, left: 20 }
-const contigHeightWidthRatio = 7 / 6
+const arrow_tip_length = 10;
+const arrow_height = 30;
+const global_top = 50;
+const geneSpacing = 5;
+const arc_pad = 0.003; //used to make sure the systems are visible on the screen
+const margin = { top: 20, right: 20, bottom: 20, left: 20 };
 
 function arrow_vertices(start, end, strand) {
-  let len = Math.abs(end - start)
-  var top
-  var mid_x
+  let len = Math.abs(end - start);
+  var top;
+  var mid_x;
 
   if (strand === 1) {
-    mid_x = start
-    top = global_top + 10
+    mid_x = start;
+    top = global_top + 10;
     if (len > arrow_tip_length) {
-      mid_x = end - arrow_tip_length
+      mid_x = end - arrow_tip_length;
     }
   } else if (strand === -1) {
-    ;[start, end] = [end, start]
-    mid_x = start
-    top = global_top + arrow_height + 20
+    [start, end] = [end, start];
+    mid_x = start;
+    top = global_top + arrow_height + 20;
     if (len > arrow_tip_length) {
-      mid_x = end + arrow_tip_length
+      mid_x = end + arrow_tip_length;
     }
   }
 
@@ -34,9 +33,9 @@ function arrow_vertices(start, end, strand) {
     [mid_x, top + arrow_height],
     [end, top + arrow_height / 2],
     [mid_x, top],
-  ]
+  ];
 
-  return result
+  return result;
 }
 
 export const drawSystems = (
@@ -45,185 +44,185 @@ export const drawSystems = (
   contig_data,
   system_data
 ) => {
-  const contigBox = d3.select(contigBoxRef)
-  const geneBox = d3.select(geneBoxRef)
+  const contigBox = d3.select(contigBoxRef);
+  const geneBox = d3.select(geneBoxRef);
 
-  const contigBoxWidth = contigBoxRef.getBoundingClientRect().width
-  const geneBoxWidth = geneBoxRef.getBoundingClientRect().width
+  const contigBoxWidth = contigBoxRef.getBoundingClientRect().width;
+  const geneBoxWidth = geneBoxRef.getBoundingClientRect().width;
 
-  const track_width = geneBoxWidth - margin.right - margin.left
+  const track_width = geneBoxWidth - margin.right - margin.left;
 
   const tooltip = geneBox
-    .append('div')
-    .style('opacity', 0)
-    .attr('class', 'tooltip')
-    .style('position', 'absolute')
-    .style('background-color', 'black')
-    .style('color', 'white')
-    .style('font-family', 'Helvetica, Arial, sans-serif')
-    .style('padding', '5px')
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("position", "absolute")
+    .style("background-color", "black")
+    .style("color", "white")
+    .style("font-family", "Helvetica, Arial, sans-serif")
+    .style("padding", "5px");
 
   const system_info = geneBox
-    .append('div')
-    .style('padding', '25px 25px 25px 25px')
-    .style('font-family', 'Helvetica, Arial, sans-serif')
+    .append("div")
+    .style("padding", "25px 25px 25px 25px")
+    .style("font-family", "Helvetica, Arial, sans-serif");
 
   const geneSvg = geneBox
-    .append('svg')
-    .attr('width', geneBoxWidth)
-    .attr('height', '100%')
+    .append("svg")
+    .attr("width", geneBoxWidth)
+    .attr("height", "100%");
 
   function clear_genes() {
-    d3.selectAll('polygon').remove()
-    d3.selectAll('gene').remove()
-    d3.selectAll('g .track').remove()
+    d3.selectAll("polygon").remove();
+    d3.selectAll("gene").remove();
+    d3.selectAll("g .track").remove();
   }
 
   function draw_genes(sys_id) {
     // Fetch the gene data associated to the system
-    let system = system_data.find((d) => d.sys_id === sys_id)
+    let system = system_data.find((d) => d.sys_id === sys_id);
     // Compute relative positions in the system's neighborhood
-    const contig_L = system.contig_length
-    const sys_start = system.neighborhood_start
-    const sys_end = system.neighborhood_end
-    const sys_L = (sys_end - sys_start) % contig_L
-    const sys_color = system.hex_code_rainbow
+    const contig_L = system.contig_length;
+    const sys_start = system.neighborhood_start;
+    const sys_end = system.neighborhood_end;
+    const sys_L = (sys_end - sys_start) % contig_L;
+    const sys_color = system.hex_code_rainbow;
 
-    let gene_data = system['genes']
+    let gene_data = system["genes"];
     gene_data = gene_data.map((obj) => ({
       ...obj,
       rel_start: ((obj.start - sys_start) * track_width) / sys_L,
-    }))
+    }));
     gene_data = gene_data.map((obj) => ({
       ...obj,
       rel_end: ((obj.end - sys_start) * track_width) / sys_L,
-    }))
+    }));
 
     // Add a first track
 
     const track = geneSvg
-      .append('g')
-      .attr('class', 'track')
-      .attr('transform', `translate(${margin.left},0)`)
+      .append("g")
+      .attr("class", "track")
+      .attr("transform", `translate(${margin.left},0)`);
 
     // Add a middle line per track
-    const linePos = global_top + arrow_height + geneSpacing + 10
+    const linePos = global_top + arrow_height + geneSpacing + 10;
     track
-      .append('line')
-      .attr('x1', 0)
-      .attr('y1', linePos)
-      .attr('x2', geneBoxWidth - margin.right - margin.left)
-      .attr('y2', linePos)
-      .style('stroke', 'black')
-      .style('stroke-width', 1)
-      .style('opacity', 0.5)
-      .style('stroke-dasharray', 4)
+      .append("line")
+      .attr("x1", 0)
+      .attr("y1", linePos)
+      .attr("x2", geneBoxWidth - margin.right - margin.left)
+      .attr("y2", linePos)
+      .style("stroke", "black")
+      .style("stroke-width", 1)
+      .style("opacity", 0.5)
+      .style("stroke-dasharray", 4);
 
     // for each track, add a "gene" element per data point
     const genes = track
-      .selectAll('g .gene')
+      .selectAll("g .gene")
       .data(gene_data)
-      .join('g')
-      .attr('class', 'gene')
-      .attr('role', (d) => d.role)
+      .join("g")
+      .attr("class", "gene")
+      .attr("role", (d) => d.role);
 
     // Add an arrow per gene
     const gene_arrows = genes
       .data(gene_data)
-      .append('polygon')
-      .attr('points', function (d) {
-        return arrow_vertices(d.rel_start, d.rel_end, d.strand).join(' ')
+      .append("polygon")
+      .attr("points", function (d) {
+        return arrow_vertices(d.rel_start, d.rel_end, d.strand).join(" ");
       })
-      .attr('class', 'gene_arrow')
-      .attr('fill', function (d) {
-        return d.role === 'non defensive' ? '#FBFCFC' : sys_color
+      .attr("class", "gene_arrow")
+      .attr("fill", function (d) {
+        return d.role === "non defensive" ? "#FBFCFC" : sys_color;
       })
-      .attr('fill-opacity', '0.5')
-      .attr('stroke-width', '1')
-      .attr('stroke', 'black')
-      .attr('strand', function (d) {
-        return d.strand
+      .attr("fill-opacity", "0.5")
+      .attr("stroke-width", "1")
+      .attr("stroke", "black")
+      .attr("strand", function (d) {
+        return d.strand;
       })
-      .attr('start', (d) => d.start)
-      .attr('end', (d) => d.end)
-      .attr('role', (d) => d.role)
+      .attr("start", (d) => d.start)
+      .attr("end", (d) => d.end)
+      .attr("role", (d) => d.role);
 
     const mouseover = (_e, _d) => {
       d3.select(this)
         .transition()
         .ease(d3.easeQuad)
         .duration(200)
-        .attr('fill-opacity', 0.9)
+        .attr("fill-opacity", 0.9);
 
-      tooltip.style('opacity', 0.8)
-    }
+      tooltip.style("opacity", 0.8);
+    };
 
     const mouseleave = (_d) => {
       d3.select(this)
         .transition()
         .ease(d3.easeQuad)
         .duration(200)
-        .attr('fill-opacity', 0.5)
+        .attr("fill-opacity", 0.5);
 
-      tooltip.style('opacity', 0)
-    }
+      tooltip.style("opacity", 0);
+    };
 
     const mousemove = (e, d) => {
       tooltip
         .html(
           `<b>Role</b>: ${d.role}<br><b>start:</b> ${d.start}<br><b>end:</b> ${d.end}`
         )
-        .style('left', d3.pointer(e, this)[0] + 70 + contigBoxWidth + 'px')
-        .style('top', d3.pointer(e, this)[1] + 'px')
-    }
+        .style("left", d3.pointer(e, this)[0] + 70 + contigBoxWidth + "px")
+        .style("top", d3.pointer(e, this)[1] + "px");
+    };
 
     // Transparency changes based on hover
     gene_arrows
-      .on('mouseover', mouseover)
-      .on('mousemove', mousemove)
-      .on('mouseleave', mouseleave)
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave);
   }
 
   // Creating the svg box of contigbox
   const contig_svg = contigBox
-    .append('svg')
-    .attr('width', contigBoxWidth)
-    .attr('height', contigBoxWidth * contigHeightWidthRatio)
+    .append("svg")
+    .attr("width", contigBoxWidth)
+    .attr("height", contigBoxWidth * contig_data.length);
 
   // Sort contigs by descending length.
   // The longest one will fill 90% of the contig box's width, and be centered horizontally.
-  contig_data.sort((a, b) => b.length - a.length)
+  contig_data.sort((a, b) => b.length - a.length);
 
   // Compute relative length to the longest contig, and relative contig radius
-  const reference_length = contig_data[0].length
-  const contig_margin = 0.05 * contigBoxWidth
+  const reference_length = contig_data[0].length;
+  const contig_margin = 0.05 * contigBoxWidth;
 
   // Create one group per contig, with id = contig.name
   const contig_groups = contig_svg
-    .selectAll('.contig-group')
+    .selectAll(".contig-group")
     .data(contig_data)
-    .join('g')
-    .attr('class', 'contig-group')
-    .attr('id', (d) => d.name)
-    .attr('fullName', (d) => d.description)
+    .join("g")
+    .attr("class", "contig-group")
+    .attr("id", (d) => d.name)
+    .attr("fullName", (d) => d.description);
 
   // Computing  length relative to the longest contig, and derive radii of contigs on the screen
   contig_data = contig_data.map((obj) => ({
     ...obj,
     relative_length: obj.length / reference_length,
-  }))
+  }));
 
   contig_data = contig_data.map((obj) => ({
     ...obj,
     radius: ((contigBoxWidth * 0.8) / 2) * obj.relative_length,
-  }))
+  }));
 
   // I compute a y_displacement per contig, preparing to apply a 'translate' operation to each contig group
-  let current_pos = 0
+  let current_pos = 0;
   for (const el of contig_data) {
-    current_pos += contig_margin + el.radius
-    el.y_displacement = current_pos
-    current_pos += contig_margin + el.radius
+    current_pos += contig_margin + el.radius;
+    el.y_displacement = current_pos;
+    current_pos += contig_margin + el.radius;
   }
 
   // move the center of the contig-groups
@@ -231,12 +230,12 @@ export const drawSystems = (
     .data(contig_data)
     .join()
     .attr(
-      'transform',
+      "transform",
       (d) => `translate(${contigBoxWidth / 2}, ${d.y_displacement})`
-    )
+    );
 
   // Draw arc
-  const arcGenerator = d3.arc().padAngle(0.02).padRadius(100).cornerRadius(4)
+  const arcGenerator = d3.arc().padAngle(0.02).padRadius(100).cornerRadius(4);
 
   const arcData = contig_data.map(function (d) {
     return {
@@ -244,16 +243,16 @@ export const drawSystems = (
       endAngle: 2 * Math.PI,
       innerRadius: d.radius - 3,
       outerRadius: d.radius + 3,
-    }
-  })
+    };
+  });
 
   contig_groups
-    .append('path')
-    .attr('class', 'contig-circle')
+    .append("path")
+    .attr("class", "contig-circle")
     .data(arcData)
-    .join('path')
-    .attr('d', arcGenerator)
-    .attr('fill', '#FBFCFC')
+    .join("path")
+    .attr("d", arcGenerator)
+    .attr("fill", "#FBFCFC");
 
   // Compute the start and end angles.
 
@@ -265,7 +264,7 @@ export const drawSystems = (
       (((obj.neighborhood_start - reference_length * arc_pad) %
         reference_length) /
         obj.contig_length),
-  }))
+  }));
 
   system_data = system_data.map((obj) => ({
     ...obj,
@@ -275,16 +274,16 @@ export const drawSystems = (
       (((obj.neighborhood_end + reference_length * arc_pad) %
         reference_length) /
         obj.contig_length),
-  }))
+  }));
 
   // Attribute systems to the corresponding groups. For each contig-group,
   // there should be a bunch of system-groups inside with there corresponding arcs
 
   for (const el of contig_data) {
-    const name = el.name
-    const radius = el.radius
+    const name = el.name;
+    const radius = el.radius;
 
-    const contig_system_data = system_data.filter((sys) => sys.contig === name)
+    const contig_system_data = system_data.filter((sys) => sys.contig === name);
 
     let arcGenerator = d3
       .arc()
@@ -292,62 +291,62 @@ export const drawSystems = (
       .padRadius(radius)
       .cornerRadius(2)
       .innerRadius(radius - 7)
-      .outerRadius(radius + 10)
+      .outerRadius(radius + 10);
 
-    const contig_id = `[id='${name}']`
+    const contig_id = `[id='${name}']`;
 
     d3.select(contig_id)
-      .selectAll('.sys-arc')
+      .selectAll(".sys-arc")
       .data(contig_system_data)
-      .join('path')
-      .attr('class', 'sys-arc')
-      .attr('id', (d) => d.sys_id)
-      .attr('d', arcGenerator)
-      .attr('fill', (d) => d.hex_code_rainbow)
-      .attr('fill-opacity', '0.7')
-      .style('cursor', 'pointer')
+      .join("path")
+      .attr("class", "sys-arc")
+      .attr("id", (d) => d.sys_id)
+      .attr("d", arcGenerator)
+      .attr("fill", (d) => d.hex_code_rainbow)
+      .attr("fill-opacity", "0.7")
+      .style("cursor", "pointer");
   }
 
   geneBox
-    .append('g')
-    .attr('class', 'namebox')
-    .attr('transform', 'translate(25,25)')
+    .append("g")
+    .attr("class", "namebox")
+    .attr("transform", "translate(25,25)");
 
   geneBox
-    .append('div')
-    .style('padding', '25px 25px 25px 25px')
-    .style('font-family', 'Helvetica, Arial, sans-serif')
+    .append("div")
+    .style("padding", "25px 25px 25px 25px")
+    .style("font-family", "Helvetica, Arial, sans-serif");
 
-  system_info.html('Click on one of the systems!')
+  system_info.html("Click on one of the systems!");
 
-  d3.selectAll('.sys-arc').on('click', function (_e, _d) {
-    clear_genes()
-    draw_genes(this.id)
-    const contigName = this.parentNode.getAttribute('fullName')
-    let disp_sys_name = this.id.replace('UserReplicon_', '').replace(/_/g, ' ')
+  d3.selectAll(".sys-arc").on("click", function (_e, _d) {
+    clear_genes();
+    draw_genes(this.id);
+    const contigName = this.parentNode.getAttribute("fullName");
+    let disp_sys_name = this.id.replace("UserReplicon_", "").replace(/_/g, " ");
     system_info.html(
       `<b>Contig:</b> ${contigName}<br><b>System</b>: ${disp_sys_name}`
-    )
-  })
+    );
+  });
 
-  d3.selectAll('.sys-arc')
-    .on('mouseover', function () {
+  d3.selectAll(".sys-arc")
+    .on("mouseover", function () {
       d3.select(this)
         .transition()
         .ease(d3.easeQuad)
         .duration(200)
-        .attr('fill-opacity', 0.9)
+        .attr("fill-opacity", 0.9);
     })
-    .on('mouseout', function () {
+    .on("mouseout", function () {
       d3.select(this)
         .transition()
         .ease(d3.easeQuad)
         .duration(200)
-        .attr('fill-opacity', 0.7)
-    })
+        .attr("fill-opacity", 0.7);
+    });
 
   return () => {
-    contigBox.selectAll('*').remove()
-    geneBox.selectAll('*').remove()
-  }
-}
+    contigBox.selectAll("*").remove();
+    geneBox.selectAll("*").remove();
+  };
+};
